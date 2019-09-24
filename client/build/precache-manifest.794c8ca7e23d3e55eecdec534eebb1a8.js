006fd734f2821711bf24ea8ce7064afb48783ede
Alvin Situ <jisitu@ucdavis.edu>
2019-09-23 18:57:46 -0700

    Add production build

---
+++ client/build/precache-manifest.794c8ca7e23d3e55eecdec534eebb1a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86998d240fd686fc1b5c393e582be11c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d9eb61262af4ee293ba",
+    "url": "/static/css/main.9c4b1159.chunk.css"
+  },
+  {
+    "revision": "d9968bbb9e13f7a88514",
+    "url": "/static/js/2.79cb187c.chunk.js"
+  },
+  {
+    "revision": "2d9eb61262af4ee293ba",
+    "url": "/static/js/main.d46c9fff.chunk.js"
+  },
+  {
+    "revision": "5d48c9f48fbff9375942",
+    "url": "/static/js/runtime~main.20cf6476.js"
+  }
+]);
